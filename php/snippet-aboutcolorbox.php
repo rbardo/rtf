a67f7ed621d0709c357f5ee344f35f913df09b6b
rbardo <rbardo@myriadcore.com>
2013-10-30 11:53:47 -0400

    colorbox styles

--- php/snippet-aboutcolorbox.php
+++ php/snippet-aboutcolorbox.php
@@ -1,5 +1,14 @@
 <div class="row" id="photogallery">
-	<div class="col-lg-3">
-		<a href="img/aboutone.jpg" class="gal"><img src="img/aboutone_tn.jpg" alt="" class="img-thumbnail"></a>
+	<div class="col-md-3 col-sm-3">
+		<a href="img/aboutone.jpg" class="gal"><img src="img/aboutone_tn.jpg" alt="" class="img-responsive img-thumbnail"></a>
+	</div>
+	<div class="col-md-3 col-sm-3">
+		<a href="img/abouttwo.jpg" class="gal" ><img src="img/abouttwo_tn.jpg" class="img-responsive img-thumbnail" alt=""></a>
+	</div>
+	<div class="col-md-3 col-sm-3">
+		<a href="img/aboutthree.jpg" class="gal"><img src="img/aboutthree_tn.jpg" class="img-responsive img-thumbnail" alt=""></a>
+	</div>
+	<div class="col-md-3 col-sm-3">
+		<a href="img/aboutfour.jpg" class="gal"><img src="img/aboutfour_tn.jpg" class="img-responsive img-thumbnail" alt=""></a>
 	</div>
 </div> <!-- colorbox row -->
